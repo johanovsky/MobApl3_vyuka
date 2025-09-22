bb04d17bac774871ca4b43f78806f32d4152cb6c
Johanovský Karel <johanovsky@prumyslovkajihlava.cz>
2025-09-22 11:18:33 +0200

    20250922 HelloWorld

---
+++ IT3A_Cjaz/HelloWorld/app.js
@@ -0,0 +1,19 @@
+if("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js")
+        .then(registration => {
+            console.log("Service Worker registered with scope: ", registration.scope);
+        })
+        .catch(error => {
+            console.log("Service Worker registration failed: ", error);
+        });
+    });
+}
+
+function sayHello() {
+    if(document.getElementById("hello").innerText === "Hello world") {
+        document.getElementById("hello").innerText = "Ahoj světe";
+    } else {
+        document.getElementById("hello").innerText = "Hello world";
+    }
+}
