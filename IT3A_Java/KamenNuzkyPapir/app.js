53808233b1492c64bede685529a00fe1583d573e
Johanovský Karel <johanovsky@prumyslovkajihlava.cz>
2025-10-03 10:31:13 +0200

    20251003

--- IT3A_Java/KamenNuzkyPapir/app.js
+++ IT3A_Java/KamenNuzkyPapir/app.js
@@ -14,7 +14,55 @@ function hra() {
     //nalezeni vybraneho radio-buttonu
     const hrac = document.querySelector("input[name='hrac']:checked").value;
     //kontrolni vypis
-    console.log(hrac);
+    console.log("Hrac:" + hrac);
+    //pocitac vybira nahodne
+    const volby = ["Kámen", "Nůžky", "Papír"];
+    const cpu = volby[Math.floor(Math.random() * volby.length)];
+    //kontrolni vypis
+    console.log("Cpu: " + cpu);
+    //vypis volby cpu
+    document.getElementById("cpu_div").innerText = cpu;
 
-    //TO DO...
+    //vyhodnoceni vysledku
+    switch(hrac) {
+        case "Kámen":
+            switch(cpu) {
+                case "Kámen":
+                    document.getElementById("result_div").innerText = "REMÍZA";
+                    break;
+                case "Nůžky":
+                    document.getElementById("result_div").innerText = "VÝHRA";
+                    break;
+                case "Papír":
+                    document.getElementById("result_div").innerText = "PROHRA";
+                    break;
+            }
+            break;
+        case "Nůžky":
+            switch(cpu) {
+                case "Kámen":
+                    document.getElementById("result_div").innerText = "PROHRA";
+                    break;
+                case "Nůžky":
+                    document.getElementById("result_div").innerText = "REMÍZA";
+                    break;
+                case "Papír":
+                    document.getElementById("result_div").innerText = "VÝHRA";
+                    break;
+            }
+            break;
+        case "Papír":
+            switch(cpu) {
+                case "Kámen":
+                    document.getElementById("result_div").innerText = "VÝHRA";
+                    break;
+                case "Nůžky":
+                    document.getElementById("result_div").innerText = "PROHRA";
+                    break;
+                case "Papír":
+                    document.getElementById("result_div").innerText = "REMÍZA";
+                    break;
+            }
+            break;       
+    }   
 }
