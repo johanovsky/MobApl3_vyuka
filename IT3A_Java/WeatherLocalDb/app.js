4c742d45442a0f64b39edacff512616f54e72410
Johanovský Karel <johanovsky@prumyslovkajihlava.cz>
2026-03-06 11:24:07 +0100

    20260306

---
+++ IT3A_Java/WeatherLocalDb/app.js
@@ -0,0 +1,176 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js")
+            .then(registration => {
+                console.log("Service Worker registered with scope:", registration.scope);
+            })
+            .catch(error => {
+                console.log("Service Worker registration failed:", error);
+            });
+    });
+}
+
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const latitude = position.coords.latitude.toFixed(4);
+            const longitude = position.coords.longitude.toFixed(4);
+            console.log("lat: " + latitude + ", lon: " + longitude);
+            getTempCoords(latitude, longitude, "city");
+        }, 
+        (error) => {
+            console.log("Chyba pri ziskavani polohy: ", error);
+        }
+    );
+} else {
+    console.log("Geolocation neni v prohlizeci podporovana");
+}
+
+const APIKEY = "ef4dab04b610b4fc6318e3a5a222fdbd";
+
+function getTemp(city, div_id) {
+    //vytahnu si bokem div
+    const div = document.getElementById(div_id);
+    //kontrola jestli mam mesto
+    if(city != "") {
+        //mam mesto budu zjistovat
+        //pozadame API o data
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKEY +"&units=metric&lang=cs")
+        .then(response => response.json())
+        .then(data => {
+            //tady bude zpracovani dat
+            if(data.cod === 200) {
+                //prisla data a jsou ok -> je v nich pocasi
+                //stahneme data
+                const zCache = data.cached;
+                const temp = data.main.temp;
+                const press = data.main.pressure;
+                const hum = data.main.humidity;
+                const icon = data.weather[0].icon;
+                //toto je v sec
+                const sunrise = data.sys.sunrise;
+                //toto chce mili-sec
+                const sunrise_date = new Date(sunrise * 1000);
+                const sunset = data.sys.sunset;
+                const sunset_date = new Date(sunset * 1000);
+
+                //slepime html do divu
+                div.innerHTML = "<h3>" + city + "</h3>";
+                if(zCache === true) {
+                    div.innerHTML += "<p>Pozor, data jsou z cache</p>";
+                }
+                div.innerHTML += "<p> <img src='https://openweathermap.org/img/wn/" + icon + ".png' /> </p>";
+                div.innerHTML += "<p>Teplota: " + temp + " °C</p>";
+                div.innerHTML += "<p>Tlak: " + press + " hPa</p>";
+                div.innerHTML += "<p>Vlhkost: " + hum + " %</p>";
+                div.innerHTML += "<p>Slunce vyjde v: " + 
+                    sunrise_date.getHours().toString().padStart(2, "0") + ":" + 
+                    sunrise_date.getMinutes().toString().padStart(2, "0") + "</p>";
+                div.innerHTML += "<p>Slunce zapadne v: " + 
+                    sunset_date.getHours().toString().padStart(2, "0") + ":" + 
+                    sunset_date.getMinutes().toString().padStart(2, "0") + "</p>";
+            } else {
+                //sice prisla data, ale je v nich nejaka chyba
+                div.innerHTML = "<p>Chyba: " + data.message + "</p>";
+            }
+        })
+        .catch(error => {
+            //sem to spadne, napr. kdyz zarizeni nebude online
+            div.innerHTML = "<p>Nastala chyba</p>";
+        });
+    } else {
+        //nemam mesto -> chyba
+        div.innerHTML = "<p>Zadej město</p>";
+    }
+}
+
+function getTempCoords(lat, lon, div_id) {
+    //vytahnu si bokem div
+    const div = document.getElementById(div_id);
+    //mam mesto budu zjistovat
+    //pozadame API o data
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY +"&units=metric&lang=cs")
+    .then(response => response.json())
+    .then(data => {
+        //tady bude zpracovani dat
+        if(data.cod === 200) {
+            //prisla data a jsou ok -> je v nich pocasi
+            //stahneme data
+            const city = data.name;
+            const temp = data.main.temp;
+            const press = data.main.pressure;
+            const hum = data.main.humidity;
+            const icon = data.weather[0].icon;
+            //toto je v sec
+            const sunrise = data.sys.sunrise;
+            //toto chce mili-sec
+            const sunrise_date = new Date(sunrise * 1000);
+            const sunset = data.sys.sunset;
+            const sunset_date = new Date(sunset * 1000);
+
+            //slepime html do divu
+            div.innerHTML = "<h3>" + city + "</h3>";
+            div.innerHTML += "<p> <img src='https://openweathermap.org/img/wn/" + icon + ".png' /> </p>";
+            div.innerHTML += "<p>Teplota: " + temp + " °C</p>";
+            div.innerHTML += "<p>Tlak: " + press + " hPa</p>";
+            div.innerHTML += "<p>Vlhkost: " + hum + " %</p>";
+            div.innerHTML += "<p>Slunce vyjde v: " + 
+                sunrise_date.getHours().toString().padStart(2, "0") + ":" + 
+                sunrise_date.getMinutes().toString().padStart(2, "0") + "</p>";
+            div.innerHTML += "<p>Slunce zapadne v: " + 
+                sunset_date.getHours().toString().padStart(2, "0") + ":" + 
+                sunset_date.getMinutes().toString().padStart(2, "0") + "</p>";
+        } else {
+            //sice prisla data, ale je v nich nejaka chyba
+            div.innerHTML = "<p>Chyba: " + data.message + "</p>";
+        }
+    })
+    .catch(error => {
+        //sem to spadne, napr. kdyz zarizeni nebude online
+        div.innerHTML = "<p>Nastala chyba</p>";
+    });
+}
+
+function getTemperature() {
+    //stahneme si mesto
+    const city = document.getElementById("input_city").value;
+    //vyplnime teplotu do zadaneho divu
+    getTemp(city, "city");
+}
+
+function addToFavorites() {
+    //najdeme si div city
+    const cityDiv = document.getElementById("city");
+    //promenne pro nazev mesta
+    let cityName = null;
+    if(cityDiv !== null) {
+        const cityH3 = cityDiv.querySelector("h3");
+        if(cityH3 !== null) {
+            cityName = cityH3.innerText;
+        }
+    }
+
+    //kdyz mame mesto
+    if(cityName !== null) {
+        //povedlo se nam vytahnout jmeno mesta
+        addFavorite(cityName)
+        .then(result => {
+            if(result) alert("Mesto " + cityName + " pridano mezi oblibene");
+            else alert(cityName + " jiz je mezi oblibenymi");
+        })
+        .catch(err => {
+            alert("Chyba pri praci s DB");
+        });
+    } else {
+        //vubec nemame mesto
+        alert("Mesto nebylo zadano");
+    }
+}
+
+window.onload = async function() {
+    //priprava DB
+    await initDB();
+
+    //zobrazeni def. mesta
+    getTemp("Dobronín", "dobronin");
+}
---
+++ IT3A_Java/WeatherLocalDb/db.js
@@ -0,0 +1,54 @@
+const DB_NAME = "weatherAppDB";
+const DB_VERSION = 1;
+const STORE_NAME = "favorites";
+ 
+let db = null;
+ 
+function initDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+ 
+    request.onupgradeneeded = (event) => {
+      const database = event.target.result;
+      if(!database.objectStoreNames.contains(STORE_NAME)) {
+        const store = database.createObjectStore(STORE_NAME, { keyPath: "id", autoIncrement: true });
+        store.createIndex("city", "city", { unique: true });
+      }
+    };
+ 
+    request.onsuccess = (event) => {
+      db = event.target.result;
+      resolve(db);
+    };
+ 
+    request.onerror = (event) => reject(event.target.error);
+  });
+}
+ 
+function addFavorite(city) {
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE_NAME, "readwrite");
+    const store = tx.objectStore(STORE_NAME);
+    const request = store.add({ city });
+    request.onsuccess = () => resolve(true);
+    request.onerror = (event) => {
+      if(event.target.error.name === "ConstraintError") resolve(false); // duplicitní město
+      else reject(event.target.error);
+    };
+  });
+}
+ 
+function getAllFavorites() {
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE_NAME, "readonly");
+    const store = tx.objectStore(STORE_NAME);
+    const request = store.getAll();
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = (event) => reject(event.target.error);
+  });
+}
+ 
+// vystavíme funkce globálně, aby byly dostupné v main.js
+window.initDB = initDB;
+window.addFavorite = addFavorite;
+window.getAllFavorites = getAllFavorites;
